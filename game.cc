1824da779ab7078e0c664012f0d3c9a87f11447a
ao094014 <31935232+ao094014@users.noreply.github.com>
2017-10-10 15:42:26 -0400

    Add files via upload

--- game.cc
+++ game.cc
@@ -17,6 +17,11 @@ namespace main_savitch_14
 //*************************************************************************
 // PUBLIC MEMBER FUNCTIONS
 
+/*!
+ * plays round of the game with human moving first
+ * @return HUMAN returns who goes next 
+ */
+
 game::who game::play( )
 // The play function should not be overridden. It plays one round of the
 // game, with the human player moving first and the computer second.
@@ -45,10 +50,20 @@ game::who game::play( )
 //*************************************************************************
 // OPTIONAL VIRTUAL FUNCTIONS (overriding these functions is optional)
 
+/*!
+ * displays a message to the players
+ * @param[out] message the message displayed 
+ */
+
 void game::display_message(const string& message) const
 {
 	cout << message;
 }
+
+/*!
+ * gets the input move from the user
+ * @return[in] answer returns the move 
+ */
 
 string game::get_user_move( ) const
 {
@@ -60,6 +75,12 @@ string game::get_user_move( ) const
 	return answer;
 }
 
+/*!
+ * determines whos winning
+ * @return[in] last_mover() return who went last
+ * @return[out] NEAUTRAL determines whos winning 
+ */
+ 
 game::who game::winning()const {
 
 	int value = evaluate();
@@ -76,6 +97,15 @@ game::who game::winning()const {
 
 //*************************************************************************
 // PRIVATE FUNCTIONS (these are the same for every game)
+
+/*!
+ * evalautes the move while looking for future moves
+ * @param[in] look_ahead evaluates the position of the boad
+ * @param[out] beat_this value of other moves 
+ * @return[out] evaluate returns evaluation of the move 
+ * @return[out] best_value returns best move available
+ * @return[out] INT_MIN returns minimum value 
+ */
 
 int game::eval_with_lookahead(int look_ahead, int beat_this)
 // Evaluate a board position with lookahead.
@@ -125,6 +155,10 @@ int game::eval_with_lookahead(int look_ahead, int beat_this)
 	return -best_value;
 }
 
+/*!
+ * makes the AI move and calculates the best move for it
+ */
+ 
 void game::make_computer_move( )
 {
 	queue<string> moves;
@@ -157,6 +191,10 @@ void game::make_computer_move( )
 	// Make the best move.
 	make_move(best_move);
 }
+
+/*!
+ * allows for the human to move if it is legal
+ */
 
 void game::make_human_move( ) {
 	string move;
